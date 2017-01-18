fc2661f3dd14b4219ce722981243877b70e4e93f
alex shyaka <shyakaster@gmail.com>
2017-01-18 16:06:23 +0300

    add and edit home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'contacts/new'
+
+  get 'contacts/create'
+
+  get 'projects/index'
+
+ get 'about/index', to: 'about#index', as: 'aboutus'
+
   root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
