7c6944c5ad3fefe670ebd5c1b762f76ad42eaf05
alex shyaka <shyakaster@gmail.com>
2017-01-19 15:27:23 +0300

    add proper header

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,7 +6,7 @@ class Contact < MailForm::Base
 
 	def headers
 		{
-			:subject => "serenitykids Contact Form",
+			:subject => "Refugee Advocacy and Development Contact Form",
 			:to => "admin@raduganda.com",
 			:from => %("#{name}" <#{email}>)
 
